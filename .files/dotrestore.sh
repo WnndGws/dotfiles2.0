197a76805389407d28ee57e19ee7f1e44856b51a
WG <wynand@gouws.com.au>
2018-07-26 14:37:11 +0800

    Log failed

--- .files/dotrestore.sh
+++ .files/dotrestore.sh
@@ -21,7 +21,7 @@ make
 sudo make install
 cd $dir; rm -rf pakku
 pakku -Syyuu
-cat ./.files/packages_unique.txt | xargs -I{} pakku -S --noconfirm --needed {}
+cat ./.files/packages_unique.txt | xargs -I{} pakku -S --noconfirm --needed {} 2>> failed_packages.txt
 
 echo "Creating new folders....."
 for folder in $(fd --exclude "*git*" --hidden --no-ignore --max-depth 1 --type d --full-path $dir -x echo {/}); do
