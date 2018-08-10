93f9f51a242e8718cb41c6d56e7815ca7ed4bcf1
WnndGws <WnndGws>
2018-08-10 09:20:53 +0800

    Be explicit with interface to avoid problems

--- .config/polybar/daily_downloads.sh
+++ .config/polybar/daily_downloads.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 ## Uses vnStat to check how much I have downloaded today
 
-vnstat --config /home/wynand/.config/vnstat/vnstat.conf --update
+vnstat --config /home/wynand/.config/vnstat/vnstat.conf --update -i wlp0s20u3
 daily_dl=$(vnstat --config /home/wynand/.config/vnstat/vnstat.conf --hours | tail -n 1 | awk '{print $10}')
 
 echo "Hourly:  $daily_dl Mb"
